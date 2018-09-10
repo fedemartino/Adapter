78efffd08ddf4fc8ad352e71f60f8cc63b9096d5
fedemartino <fedemartino@gmail.com>
2018-09-10 19:29:12 -0300

    german tv

---
+++ GermanTV.cs
@@ -0,0 +1,19 @@
+namespace Adapater
+{
+    class GermanTV
+    {
+        string status = "eingeschaltet";
+        public void Ausschalten()
+        {
+            this.status = "ausgeschaltet";
+        }
+        public void  Einschalten()
+        {
+            this.status = "eingeschaltet";
+        }
+        public string Status
+        {
+            get {return this.status;}
+        }
+    }
+}
