7b0fadc1ddb6707a08f538260e9c793b5b144baf
fedemartino <fedemartino@gmail.com>
2018-09-10 18:28:39 -0300

    Initial Commit

---
+++ ISmartTV.cs
@@ -0,0 +1,5 @@
+interface ISmartTV
+{
+    void PlayNetflix();
+    void PlayYoutube();
+}
---
+++ ITV.cs
@@ -0,0 +1,16 @@
+interface ITV
+{
+    /// <summary>
+    /// Prende la TV
+    /// </summary>
+    void TurnOn();
+    /// <summary>
+    /// Apaga la TV
+    /// </summary>
+    void TurnOff();
+    /// <summary>
+    /// Devuelve el estado de la TV
+    /// </summary>
+    /// <returns>True si la TV está encendida</returns>
+    bool Status();
+}
